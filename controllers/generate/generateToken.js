e25658fe5287ff95c3c9e590b4a3bbdc2828f89a
Aijeysh Sharma <88200722+Aijeysh@users.noreply.github.com>
2023-06-14 09:40:01 +0545

    Update generateToken.js

--- controllers/generate/generateToken.js
+++ controllers/generate/generateToken.js
@@ -2,14 +2,15 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWTSECRET, {
-    expiresIn: '7d'
+    expiresIn: '7d',
   });
 
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000
+    sameSite: 'none',
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    domain: 'waysprivate.com.np', // Replace with the desired domain
   })
 }
 
